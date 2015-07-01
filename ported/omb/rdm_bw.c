d89928547a70ee6bdcc25179a9ac5b65c6d66dad
James Swaro <jswaro@cray.com>
2015-07-01 13:09:47 -0500

    first pass.... PMI_Barrier is crashing

--- ported/omb/rdm_bw.c
+++ ported/omb/rdm_bw.c
@@ -46,6 +46,8 @@
 #include <rdma/fi_cm.h>
 #include <rdma/fi_rma.h>
 
+#include <pthread.h>
+
 #include "ft_utils.h"
 #include "shared.h"
 
@@ -72,21 +74,26 @@ int skip_large = 2;
 
 int large_message_size = 8192;
 
-char s_buf_original[MYBUFSIZE];
-char r_buf_original[MYBUFSIZE];
-
 static int rx_depth = 512;
 
+struct per_thread_data {
+	pthread_t thread;
+	int tid; /* thread id */
+	struct fid_ep *ep;
+	struct fid_av *av;
+	struct fid_cq *rcq, *scq;
+	struct fid_mr *r_mr, *l_mr;
+	struct fi_context fi_ctx_send;
+	struct fi_context fi_ctx_recv;
+	struct fi_context fi_ctx_av;
+	char s_buf_original[MYBUFSIZE];
+	char r_buf_original[MYBUFSIZE];
+};
+
+struct per_thread_data *thread_data;
 struct fi_info *fi, *hints;
 struct fid_fabric *fab;
 struct fid_domain *dom;
-struct fid_ep *ep;
-struct fid_av *av;
-struct fid_cq *rcq, *scq;
-struct fid_mr *r_mr, *l_mr;
-struct fi_context fi_ctx_send;
-struct fi_context fi_ctx_recv;
-struct fi_context fi_ctx_av;
 
 void *addrs;
 fi_addr_t *fi_addrs;
@@ -100,20 +107,119 @@ buf_desc_t *rbuf_descs;
 
 int myid, numprocs;
 
+struct test_tunables {
+	int threads;
+};
+
+enum {
+	ENV_TYPE_INT = 1,
+	ENV_TYPE_BOOL,
+	ENV_TYPE_STR,
+};
+
+enum {
+	RDM_BW_THREADS = 0,
+	RDM_MAX_ENV_CNT
+};
+
+struct environ_var {
+	const char* name;
+	int type;
+	union _defaults {
+		void *addr;
+		int int_val;
+		int bool_val;
+		char *str_val;
+	} def; /* defaults */
+	union _tunables{
+		void **addr;
+		int *int_val;
+		int *bool_val;
+		char **str_val;
+	} out;
+};
+
+struct test_tunables tunables;
+struct environ_var env_vars[RDM_MAX_ENV_CNT] = {
+		{ .name = "RDM_BW_THREADS",
+		  .type = ENV_TYPE_INT,
+		  .def.int_val = 1,
+		  .out.addr = (void *) &tunables.threads,
+		},
+};
+
+int threads_in_barrier;
+pthread_cond_t done;
+pthread_mutex_t mutex;
+
+static void get_environment_variables(void)
+{
+	struct environ_var *var;
+	char *value = NULL, *tmp, *end;
+	int i;
+
+	for (i = 0; i < RDM_MAX_ENV_CNT; i++)
+	{
+		var = &env_vars[i];
+		if (!var->def.addr)
+			continue;
+
+		switch (var->type) {
+		case ENV_TYPE_INT:
+			*var->out.int_val = var->def.int_val;
+			break;
+		case ENV_TYPE_BOOL:
+			*var->out.bool_val = (var->def.bool_val != 0);
+			break;
+		case ENV_TYPE_STR:
+			strcpy(*var->out.str_val, var->def.str_val);
+			break;
+		}
+
+		value = getenv(var->name);
+		if (!value)
+			continue;
+
+		tmp = strdup(value);
+		if (!tmp)
+			continue;
+
+		switch (var->type) {
+		case ENV_TYPE_INT:
+			*var->out.int_val = strtol(tmp, &end, 10);
+			if (errno == EINVAL || errno == ERANGE)
+				*var->out.int_val = var->def.int_val;
+			break;
+		case ENV_TYPE_BOOL:
+			*var->out.bool_val = strtol(tmp, &end, 10);
+			if (errno == EINVAL || errno == ERANGE)
+				*var->out.bool_val = var->def.bool_val;
+
+			*var->out.bool_val = (*var->out.bool_val != 0);
+			break;
+		case ENV_TYPE_STR:
+			strcpy(*var->out.str_val, tmp);
+			break;
+		}
+
+		free(tmp);
+	}
+}
+
 void print_usage(void)
 {
 	if (!myid)
 		ft_basic_usage(TEST_DESC);
 }
 
-static void free_ep_res(void)
+static void free_ep_res(struct per_thread_data *ptd)
 {
-	fi_close(&av->fid);
-	fi_close(&rcq->fid);
-	fi_close(&scq->fid);
+	fi_close(&ptd->av->fid);
+	fi_close(&ptd->rcq->fid);
+	fi_close(&ptd->scq->fid);
 }
 
-static int alloc_ep_res(void)
+static int alloc_ep_res(struct per_thread_data *ptd)
 {
 	struct fi_cq_attr cq_attr;
 	struct fi_av_attr av_attr;
@@ -125,14 +231,14 @@ static int alloc_ep_res(void)
 	cq_attr.size = rx_depth;
 
 	/* Open completion queue for send completions */
-	ret = fi_cq_open(dom, &cq_attr, &scq, NULL);
+	ret = fi_cq_open(dom, &cq_attr, &ptd->scq, NULL);
 	if (ret) {
 		FT_PRINTERR("fi_cq_open", ret);
 		goto err1;
 	}
 
 	/* Open completion queue for recv completions */
-	ret = fi_cq_open(dom, &cq_attr, &rcq, NULL);
+	ret = fi_cq_open(dom, &cq_attr, &ptd->rcq, NULL);
 	if (ret) {
 		FT_PRINTERR("fi_cq_open", ret);
 		goto err2;
@@ -145,7 +251,7 @@ static int alloc_ep_res(void)
 	av_attr.name = NULL;
 
 	/* Open address vector (AV) for mapping address */
-	ret = fi_av_open(dom, &av_attr, &av, NULL);
+	ret = fi_av_open(dom, &av_attr, &ptd->av, NULL);
 	if (ret) {
 		FT_PRINTERR("fi_av_open", ret);
 		 goto err3;
@@ -154,39 +260,39 @@ static int alloc_ep_res(void)
 	return 0;
 
 err3:
-	fi_close(&rcq->fid);
+	fi_close(&ptd->rcq->fid);
 err2:
-	fi_close(&scq->fid);
+	fi_close(&ptd->scq->fid);
 err1:
 	return ret;
 }
 
-static int bind_ep_res(void)
+static int bind_ep_res(struct per_thread_data *ptd)
 {
 	int ret;
 
 	/* Bind Send CQ with endpoint to collect send completions */
-	ret = fi_ep_bind(ep, &scq->fid, FI_SEND|FI_WRITE);
+	ret = fi_ep_bind(ptd->ep, &ptd->scq->fid, FI_SEND|FI_WRITE);
 	if (ret) {
 		FT_PRINTERR("fi_ep_bind", ret);
 		return ret;
 	}
 
 	/* Bind Recv CQ with endpoint to collect recv completions */
-	ret = fi_ep_bind(ep, &rcq->fid, FI_RECV);
+	ret = fi_ep_bind(ptd->ep, &ptd->rcq->fid, FI_RECV);
 	if (ret) {
 		FT_PRINTERR("fi_ep_bind", ret);
 		return ret;
 	}
 
 	/* Bind AV with the endpoint to map addresses */
-	ret = fi_ep_bind(ep, &av->fid, 0);
+	ret = fi_ep_bind(ptd->ep, &ptd->av->fid, 0);
 	if (ret) {
 		FT_PRINTERR("fi_ep_bind", ret);
 		return ret;
 	}
 
-	ret = fi_enable(ep);
+	ret = fi_enable(ptd->ep);
 	if (ret) {
 		FT_PRINTERR("fi_enable", ret);
 		return ret;
@@ -221,47 +327,56 @@ static int init_fabric(void)
 		goto err2;
 	}
 
-	/* Open endpoint */
-	ret = fi_endpoint(dom, fi, &ep, NULL);
-	if (ret) {
-		FT_PRINTERR("fi_endpoint", ret);
-		goto err3;
-	}
-
-	/* Allocate endpoint resources */
-	ret = alloc_ep_res();
-	if (ret)
-		goto err4;
-
-	/* Bind EQs and AVs with endpoint */
-	ret = bind_ep_res();
-	if (ret)
-		goto err5;
-
 	return 0;
 
-err5:
-	free_ep_res();
-err4:
-	fi_close(&ep->fid);
-err3:
-	fi_close(&dom->fid);
 err2:
 	fi_close(&fab->fid);
 err1:
 	return ret;
 }
 
-static int init_av(void)
+static int init_endpoint(struct per_thread_data *ptd)
+{
+	int ret;
+
+	/* Open endpoint */
+	ret = fi_endpoint(dom, fi, &ptd->ep, NULL);
+	if (ret) {
+		FT_PRINTERR("fi_endpoint", ret);
+		goto err3;
+	}
+
+	/* Allocate endpoint resources */
+	ret = alloc_ep_res(ptd);
+	if (ret)
+		goto err4;
+
+	/* Bind EQs and AVs with endpoint */
+	ret = bind_ep_res(ptd);
+	if (ret)
+		goto err5;
+
+	return 0;
+
+err5:
+	free_ep_res(ptd);
+err4:
+	fi_close(&ptd->ep->fid);
+err3:
+	fi_close(&dom->fid);
+	return ret;
+}
+
+static int init_av(struct per_thread_data *ptd)
 {
 	void *addr;
 	size_t addrlen = 0;
 	int ret;
 
-	fi_getname(&ep->fid, NULL, &addrlen);
+	fi_getname(&ptd->ep->fid, NULL, &addrlen);
 	addr = malloc(addrlen);
 	assert(addr);
-	ret = fi_getname(&ep->fid, addr, &addrlen);
+	ret = fi_getname(&ptd->ep->fid, addr, &addrlen);
 	if (ret != 0) {
 		FT_PRINTERR("fi_getname", ret);
 		return ret;
@@ -276,7 +391,7 @@ static int init_av(void)
 	assert(fi_addrs);
 
 	/* Insert address to the AV and get the fabric address back */
-	ret = fi_av_insert(av, addrs, numprocs, fi_addrs, 0, &fi_ctx_av);
+	ret = fi_av_insert(ptd->av, addrs, numprocs, fi_addrs, 0, &ptd->fi_ctx_av);
 	if (ret != numprocs) {
 		FT_PRINTERR("fi_av_insert", ret);
 		return ret;
@@ -287,15 +402,196 @@ static int init_av(void)
 	return 0;
 }
 
-int main(int argc, char *argv[])
+void threaded_FT_Barrier(void)
+{
+	int val;
+
+	pthread_mutex_lock(&mutex);
+	val = ++threads_in_barrier;
+    if (val == tunables.threads) {
+    	FT_Barrier();
+    	pthread_cond_signal(&done);
+    } else {
+    	pthread_cond_wait(&done, &mutex);
+    }
+    val = --threads_in_barrier;
+    pthread_mutex_unlock(&mutex);
+
+    if (val == 0)
+    	pthread_cond_init(&done, NULL);
+}
+
+int thread_fn(void *data)
 {
 	int i, j, peer;
 	int size, align_size;
 	char *s_buf, *r_buf;
 	uint64_t t_start = 0, t_end = 0, t = 0;
-	int op, ret;
+	int ret;
 	buf_desc_t lbuf_desc;
 	ssize_t fi_rc;
+	int ptd_index = (int) data;
+	struct per_thread_data *ptd;
+
+	if (ptd_index >= tunables.threads)
+		return -EINVAL;
+
+	ptd = &thread_data[ptd_index];
+
+	ret = init_endpoint(ptd);
+	if (ret) {
+		fprintf(stderr, "Problem in endpoint initialization\n");
+		return ret;
+	}
+
+	ret = init_av(ptd);
+	if (ret) {
+		fprintf(stderr, "Problem in AV initialization\n");
+		return ret;
+	}
+
+	/* Data initialization */
+	align_size = getpagesize();
+	assert(align_size <= MAX_ALIGNMENT);
+
+	s_buf = (char *) (((unsigned long) ptd->s_buf_original + (align_size - 1)) /
+				align_size * align_size);
+	r_buf = (char *) (((unsigned long) ptd->r_buf_original + (align_size - 1)) /
+				align_size * align_size);
+
+	ret = fi_mr_reg(dom, r_buf, MYBUFSIZE, FI_REMOTE_WRITE, 0, 0, 0, &ptd->r_mr, NULL);
+	if (ret) {
+		FT_PRINTERR("fi_mr_reg", ret);
+		return -1;
+	}
+
+	lbuf_desc.addr = (uint64_t) r_buf;
+	lbuf_desc.key = fi_mr_key(ptd->r_mr);
+
+	rbuf_descs = (buf_desc_t *) malloc(numprocs * sizeof(buf_desc_t));
+
+	/* Distribute memory keys */
+	FT_Allgather(&lbuf_desc, sizeof(lbuf_desc), rbuf_descs);
+
+	ret = fi_mr_reg(dom, s_buf, MYBUFSIZE, FI_WRITE, 0, 0, 0, &ptd->l_mr, NULL);
+	if (ret) {
+		FT_PRINTERR("fi_mr_reg", ret);
+		return -1;
+	}
+
+	if (myid == 0) {
+		fprintf(stdout, HEADER);
+		fprintf(stdout, "%-*s%*s\n", 10, "# Size", FIELD_WIDTH,
+				"Bandwidth (MB/s)");
+		fflush(stdout);
+	}
+
+	/* Bandwidth test */
+	for (size = 1; size <= MAX_MSG_SIZE; size *= 2) {
+		/* touch the data */
+		for (i = 0; i < size; i++) {
+			s_buf[i] = 'a';
+			r_buf[i] = 'b';
+		}
+
+		if (size > large_message_size) {
+			loop = loop_large;
+			skip = skip_large;
+			window_size = window_size_large;
+		}
+
+		threaded_FT_Barrier();
+
+		if (myid == 0) {
+			peer = 1;
+
+			for (i = 0; i < loop + skip; i++) {
+				if (i == skip) {
+					t_start = get_time_usec();
+				}
+
+				for (j = 0; j < window_size; j++) {
+					fi_rc = fi_write(ptd->ep, s_buf, size, ptd->l_mr,
+							fi_addrs[peer],
+							rbuf_descs[peer].addr,
+							rbuf_descs[peer].key,
+							(void *)(intptr_t)j);
+					if (fi_rc) {
+						FT_PRINTERR("fi_write", fi_rc);
+						return fi_rc;
+					}
+				}
+
+				wait_for_data_completion(ptd->scq, window_size);
+			}
+
+			fi_rc = fi_send(ptd->ep, s_buf, 4, NULL,
+					fi_addrs[peer],
+					NULL);
+			assert(!fi_rc);
+			wait_for_data_completion(ptd->scq, 1);
+
+			fi_rc = fi_recv(ptd->ep, s_buf, 4, NULL,
+					fi_addrs[peer],
+					NULL);
+			assert(!fi_rc);
+			wait_for_data_completion(ptd->rcq, 1);
+
+			t_end = get_time_usec();
+			t = t_end - t_start;
+		} else if (myid == 1) {
+			peer = 0;
+
+			fi_rc = fi_recv(ptd->ep, s_buf, 4, NULL,
+					fi_addrs[peer],
+					NULL);
+			assert(!fi_rc);
+			wait_for_data_completion(ptd->rcq, 1);
+
+			fi_rc = fi_send(ptd->ep, s_buf, 4, NULL,
+					fi_addrs[peer],
+					NULL);
+			assert(!fi_rc);
+			wait_for_data_completion(ptd->scq, 1);
+		}
+
+		if (myid == 0) {
+			double tmp = size / 1e6 * loop * window_size;
+
+			fprintf(stdout, "%-*d%*.*f\n", 10, size, FIELD_WIDTH,
+					FLOAT_PRECISION, tmp / (t / 1e6));
+			fflush(stdout);
+		}
+	}
+
+	threaded_FT_Barrier();
+
+	fi_close(&ptd->l_mr->fid);
+	fi_close(&ptd->r_mr->fid);
+
+	free_ep_res(ptd);
+
+	fi_close(&ptd->ep->fid);
+
+	return 0;
+}
+
+int main(int argc, char *argv[])
+{
+	int op, ret;
+	int i;
+
+	pthread_mutex_init(&mutex, NULL);
+	pthread_cond_init(&done, NULL);
+
+	get_environment_variables();
+	printf("%i thread_data\n", tunables.threads);
+
+	thread_data = calloc(tunables.threads, sizeof(struct per_thread_data));
+	if (!thread_data) {
+		fprintf(stderr, "Could not allocate memory for per thread struct\n");
+		return -1;
+	}
 
 	FT_Init(&argc, &argv);
 	FT_Rank(&myid);
@@ -337,134 +633,17 @@ int main(int argc, char *argv[])
 		return ret;
 	}
 
-	ret = init_av();
-	if (ret) {
-		fprintf(stderr, "Problem in AV initialization\n");
-		return ret;
+	/* threaded section */
+	for (i = 0; i < tunables.threads; i++) {
+		ret = pthread_create(&thread_data[i].thread, NULL, thread_fn, (void *) i);
+		if (ret != 0)
+			pthread_exit(NULL); /* a more robust exit would be nice here */
 	}
 
-	/* Data initialization */
-	align_size = getpagesize();
-	assert(align_size <= MAX_ALIGNMENT);
+	for (i = 0; i < tunables.threads; i++)
+		pthread_join(thread_data[i].thread, NULL);
 
-	s_buf = (char *) (((unsigned long) s_buf_original + (align_size - 1)) /
-				align_size * align_size);
-	r_buf = (char *) (((unsigned long) r_buf_original + (align_size - 1)) /
-				align_size * align_size);
-
-	ret = fi_mr_reg(dom, r_buf, MYBUFSIZE, FI_REMOTE_WRITE, 0, 0, 0, &r_mr, NULL);
-	if (ret) {
-		FT_PRINTERR("fi_mr_reg", ret);
-		return -1;
-	}
-
-	lbuf_desc.addr = (uint64_t)r_buf;
-	lbuf_desc.key = fi_mr_key(r_mr);
-
-	rbuf_descs = (buf_desc_t *)malloc(numprocs * sizeof(buf_desc_t));
-
-	/* Distribute memory keys */
-	FT_Allgather(&lbuf_desc, sizeof(lbuf_desc), rbuf_descs);
-
-	ret = fi_mr_reg(dom, s_buf, MYBUFSIZE, FI_WRITE, 0, 0, 0, &l_mr, NULL);
-	if (ret) {
-		FT_PRINTERR("fi_mr_reg", ret);
-		return -1;
-	}
-
-	if (myid == 0) {
-		fprintf(stdout, HEADER);
-		fprintf(stdout, "%-*s%*s\n", 10, "# Size", FIELD_WIDTH,
-				"Bandwidth (MB/s)");
-		fflush(stdout);
-	}
-
-	/* Bandwidth test */
-	for (size = 1; size <= MAX_MSG_SIZE; size *= 2) {
-		/* touch the data */
-		for (i = 0; i < size; i++) {
-			s_buf[i] = 'a';
-			r_buf[i] = 'b';
-		}
-
-		if (size > large_message_size) {
-			loop = loop_large;
-			skip = skip_large;
-			window_size = window_size_large;
-		}
-
-		FT_Barrier();
-
-		if (myid == 0) {
-			peer = 1;
-
-			for (i = 0; i < loop + skip; i++) {
-				if (i == skip) {
-					t_start = get_time_usec();
-				}
-
-				for (j = 0; j < window_size; j++) {
-					fi_rc = fi_write(ep, s_buf, size, l_mr,
-							fi_addrs[peer],
-							rbuf_descs[peer].addr,
-							rbuf_descs[peer].key,
-							(void *)(intptr_t)j);
-					if (fi_rc) {
-						FT_PRINTERR("fi_write", fi_rc);
-						return fi_rc;
-					}
-				}
-
-				wait_for_data_completion(scq, window_size);
-			}
-
-			fi_rc = fi_send(ep, s_buf, 4, NULL,
-					fi_addrs[peer],
-					NULL);
-			assert(!fi_rc);
-			wait_for_data_completion(scq, 1);
-
-			fi_rc = fi_recv(ep, s_buf, 4, NULL,
-					fi_addrs[peer],
-					NULL);
-			assert(!fi_rc);
-			wait_for_data_completion(rcq, 1);
-
-			t_end = get_time_usec();
-			t = t_end - t_start;
-		} else if (myid == 1) {
-			peer = 0;
-
-			fi_rc = fi_recv(ep, s_buf, 4, NULL,
-					fi_addrs[peer],
-					NULL);
-			assert(!fi_rc);
-			wait_for_data_completion(rcq, 1);
-
-			fi_rc = fi_send(ep, s_buf, 4, NULL,
-					fi_addrs[peer],
-					NULL);
-			assert(!fi_rc);
-			wait_for_data_completion(scq, 1);
-		}
-
-		if (myid == 0) {
-			double tmp = size / 1e6 * loop * window_size;
-
-			fprintf(stdout, "%-*d%*.*f\n", 10, size, FIELD_WIDTH,
-					FLOAT_PRECISION, tmp / (t / 1e6));
-			fflush(stdout);
-		}
-	}
-
-	FT_Barrier();
-
-	fi_close(&l_mr->fid);
-	fi_close(&r_mr->fid);
-
-	free_ep_res();
-
-	fi_close(&ep->fid);
+	/* end of threaded section */
 	fi_close(&dom->fid);
 	fi_close(&fab->fid);
 
@@ -473,7 +652,8 @@ int main(int argc, char *argv[])
 
 	FT_Barrier();
 	FT_Finalize();
-	return 0;
+
+	pthread_exit(NULL);
 }
 
 /* vi:set sw=8 sts=8 */
